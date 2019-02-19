5bc673558c73bf19692c9c0ef1cf15bd5b74be86
utzcoz <utzcoz@outlook.com>
2019-02-19 23:36:12 +0800

    Use tsinghua ruby gem to speed downloading
    Signed-off-by: utzcoz <utzcoz@outlook.com>

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Use tsinghua tuna ruby gem source
+bundle config mirror.https://rubygems.org https://mirrors.tuna.tsinghua.edu.cn/rubygems/
 bundle install
 bundle exec jekyll serve
