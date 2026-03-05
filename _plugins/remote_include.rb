bd2136166997e67e6d174bb308b53fdeef835bf2
utzcoz <utzcoz@outlook.com>
2026-03-05 20:11:47 +0800

    Add customized remote-include
    The outer one has deprecated a long time.
    
    Signed-off-by: utzcoz <utzcoz@outlook.com>

---
+++ _plugins/remote_include.rb
@@ -0,0 +1,36 @@
+require 'net/http'
+require 'uri'
+
+module Jekyll
+  class RemoteInclude < Liquid::Tag
+    def initialize(tag_name, url, tokens)
+      super
+      @url = url.strip
+    end
+
+    def render(context)
+      # Resolve variable references or use literal URL
+      url = context[@url] || @url
+      uri = URI.parse(url.strip)
+      fetch(uri)
+    end
+
+    private
+
+    def fetch(uri, redirect_limit = 5)
+      raise "Too many redirects" if redirect_limit == 0
+
+      response = Net::HTTP.get_response(uri)
+      case response
+      when Net::HTTPSuccess
+        response.body.force_encoding('utf-8')
+      when Net::HTTPRedirection
+        fetch(URI.parse(response['location']), redirect_limit - 1)
+      else
+        raise "Failed to fetch #{uri}: #{response.code} #{response.message}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('remote_include', Jekyll::RemoteInclude)
