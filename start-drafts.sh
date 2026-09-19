b589f035153e77201e4bdb9aed7477260a9c9713
utzcoz <utzcoz@outlook.com>
2026-09-19 12:14:33 +0800

    Remove the Tsinghua RubyGems mirror from start scripts
    start.sh and start-drafts.sh ran `bundle config mirror` on every start,
    pointing rubygems.org at mirrors.tuna.tsinghua.edu.cn. Drop it so the
    scripts use the Gemfile's rubygems.org source directly.

--- start-drafts.sh
+++ start-drafts.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Use tsinghua tuna ruby gem source
-bundle config mirror.https://rubygems.org https://mirrors.tuna.tsinghua.edu.cn/rubygems/
 bundle install
 # We should run follow commented command to fix ffi compatibility problem on M1.
 # gem install --user-install ffi -- --enable-libffi-alloc
--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-# Use tsinghua tuna ruby gem source
-bundle config mirror.https://rubygems.org https://mirrors.tuna.tsinghua.edu.cn/rubygems/
 bundle install
 bundle exec jekyll serve --watch --trace
